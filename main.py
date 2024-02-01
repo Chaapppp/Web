bf808606c62ac7398e77b5486c2c663593a85b80
Chaapppp <chanisarappp@gmail.com>
2024-02-01 23:08:23 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    return app
+
+app = create_app()
+if __name__ == '__main__':
+    app.run(debug=True)
